72615ca7272952f96006f02c092bff59c45b7234
David Bowles <david.bowles84272@outlook.com>
2024-02-05 14:45:24 +0000

    Added logo's, static and media settings

--- amelia_mae/settings.py
+++ amelia_mae/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-bowlesy666-ameliamaecos-1vyy92zunob.ws-eu107.gitpod.io']
+ALLOWED_HOSTS = ['8000-bowlesy666-ameliamaecos-1vyy92zunob.ws-eu108.gitpod.io']
 
 
 # Application definition
@@ -147,6 +147,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
