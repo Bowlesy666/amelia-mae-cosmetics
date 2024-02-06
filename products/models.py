25c545f9a099c296ada6d757d2c7f372a7430c11
David Bowles <david.bowles84272@outlook.com>
2024-02-06 16:27:54 +0000

    Added product models, migrated and installed pillow

--- products/models.py
+++ products/models.py
@@ -1,3 +1,72 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+
+class Category(models.Model):
+    """ Model representing the product category """
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        """ String to represent the Category object """
+        return self.name
+
+    def get_friendly_name(self):
+        """ Method to return the friendly name of the Category """
+        return self.friendly_name
+
+
+class SkinType(models.Model):
+    """ Model representing the skin types associated with products """
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        """ String to represent the SkinType object """
+        return self.name
+
+    def get_friendly_name(self):
+        """ Method to return the friendly name of the skin type """
+        return self.friendly_name
+
+
+class Reviews(models.Model):
+    """ Model representing the product reviews """
+    product = models.ForeignKey(
+        'Product', on_delete=models.CASCADE, related_name='reviews')
+    user = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True, blank=True)
+    comment = models.CharField(max_length=254, null=True, blank=True)
+    rating = models.DecimalField(
+        max_digits=2, decimal_places=0,
+        validators=[MinValueValidator(0),
+        MaxValueValidator(5)]
+    )
+
+    def __str__(self):
+        """ String to represent the Reviews Object """
+        return f"Review by {self.user.username}: {self.comment[:50]}..."
+
+
+class Product(models.Model):
+    """ Model representing a Product """
+    category = models.ForeignKey(
+        'Category', null=True, blank=True, on_delete=models.SET_NULL
+    )
+    skin_type = models.ForeignKey(
+        'SkinType', on_delete=models.SET_NULL,
+        max_length=100, null=True, blank=True)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.DecimalField(
+        max_digits=6, decimal_places=2, null=True, blank=True
+    )
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        """ String to represent the Product object """
+        return self.name
