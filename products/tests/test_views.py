ee190859343a9d9463d04ddadcf9d2baed88b7d3
David Bowles <david.bowles84272@outlook.com>
2024-02-08 12:45:07 +0000

    Updated test views, setUp and tearDown

--- products/tests/test_views.py
+++ products/tests/test_views.py
@@ -5,12 +5,26 @@ from products.models import Product, Category, Reviews, SkinType
 
 class TestProductViews(TestCase):
     """ Test views for the Products app """
+    def setUp(self):
+        # Create a Product instance in the test database
+        self.product = Product.objects.create(
+            name='Test Product',
+            description='Test Description',
+            price=10.00,
+            image='image.png'
+        )
+
     def test_products_list_render(self):
         response = self.client.get('/products/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'products/products_list.html')
 
     def test_product_detail_render(self):
-        response = self.client.get('/products/1/')
+        product = Product()
+        response = self.client.get(f'/products/{self.product.id}/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'products/product_detail.html')
+
+    def tearDown(self):
+        # Delete the Product instance from the test database
+        self.product.delete()
