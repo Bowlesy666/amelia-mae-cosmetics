f016a69f5e5c5969b0c1947d626425f7bafc87c1
David Bowles <david.bowles84272@outlook.com>
2024-02-08 11:23:31 +0000

    Added product detail unit test

--- products/tests/test_views.py
+++ products/tests/test_views.py
@@ -9,3 +9,8 @@ class TestProductViews(TestCase):
         response = self.client.get('/products/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'products/products_list.html')
+
+    def test_product_detail_render(self):
+        response = self.client.get('/products/1/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'products/product_detail.html')
