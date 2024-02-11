04b5116031ec5806d9964a842b436e53dd447b6b
David Bowles <david.bowles84272@outlook.com>
2024-02-11 15:11:56 +0000

    update contexts.py bag_items

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,13 +1,26 @@
 from decimal import Decimal
+from django.shortcuts import get_object_or_404
 from django.conf import settings
+from products.models import Product
 
 
-def bag_contents(requests):
+def bag_contents(request):
 
     bag_items = []
     # initialise counts
     total = 0
     product_count = 0
+    bag = request.session.get('bag', {})
+
+    for item_id, quantity in bag.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         # using decimal instead of float is said to be more accurate and less problematic
