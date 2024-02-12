369d32828eb8b17acad42424a8e67e393fb75bf3
David Bowles <david.bowles84272@outlook.com>
2024-02-12 21:33:17 +0000

    Added stripe keys to view

--- checkout/views.py
+++ checkout/views.py
@@ -1,22 +1,43 @@
+import os
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
-import os
+from django.conf import settings
+
 
 from .forms import OrderForm
+from bag.contexts import bag_contents
+
+import stripe
 
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "Your shopping bag is empty")
         return redirect(reverse('products'))
 
+    current_bag = bag_contents(request)
+    total = current_bag['grand_total']
+    stripe_total = round(total*100)
+    stripe.api_key = stripe_secret_key
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. \
+            Contact Bowles Developmenmt to rport this issue 07783409714')
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': os.environ.get('STRIPE_PUBLIC_KEY'),
-        'client_secret': os.environ.get('STRIPE_SECRET_KEY'),
+        'stripe_public_key': stripe_public_key,
+        'client_secret': stripe_secret_key,
     }
 
     return render(request, template, context)
