0040e9d9cf2466f820fcd36b9cc722946b2a8548
David Bowles <david.bowles84272@outlook.com>
2024-02-13 11:46:27 +0000

    Added cache_checkout view/url & user detail handling in js

--- checkout/urls.py
+++ checkout/urls.py
@@ -5,5 +5,6 @@ from .webhooks import webhook
 urlpatterns = [
     path('', views.checkout, name='checkout'),
     path('checkout_success/<order_number>/', views.checkout_success, name='checkout_success'),
+    path('cache_chekcout_data/', views.cache_chekcout_data, name='cache_chekcout_data'),
     path('wh/', webhook, name='webhook'),
 ]
