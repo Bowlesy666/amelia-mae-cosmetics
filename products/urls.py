5298143b14fd6b02691801b5d0e92a46575b1676
David Bowles <david.bowles84272@outlook.com>
2024-02-15 21:50:58 +0000

    Added delete product functionality

--- products/urls.py
+++ products/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('product_admin/', views.product_admin, name='product_admin'),
     path('add/', views.add_product, name='add_product'),
     path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
+    path('delete/<int:product_id>/', views.delete_product, name='delete_product'),
 ]
