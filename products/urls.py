f4a5ab04d7211bc1cc71a0315152cb976d215f43
David Bowles <david.bowles84272@outlook.com>
2024-02-15 21:30:37 +0000

    Added edit products functionality & product admin link

--- products/urls.py
+++ products/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('<int:product_id>/', views.product_detail, name='product_detail'),
     path('product_admin/', views.product_admin, name='product_admin'),
     path('add/', views.add_product, name='add_product'),
+    path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
 ]
