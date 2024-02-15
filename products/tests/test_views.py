10459148970b2b1c20301a9b5bed2db56264150b
David Bowles <david.bowles84272@outlook.com>
2024-02-15 12:02:36 +0000

    Added test views - queries, categories, sort

--- products/tests/test_views.py
+++ products/tests/test_views.py
@@ -7,13 +7,25 @@ class TestProductViews(TestCase):
     """ Test views for the Products app """
     def setUp(self):
         # Create a Product instance in the test database
+        self.category = Category.objects.create(name='Test Category')
         self.product = Product.objects.create(
             name='Test Product',
             description='Test Description',
             price=10.00,
             image='image.png',
             is_best_seller=True,
+            category=self.category
         )
+        self.category_two = Category.objects.create(name='Test Category Two')
+        self.product_two = Product.objects.create(
+            name='Test Product',
+            description='Test Description',
+            price=50.00,
+            image='image.png',
+            is_best_seller=True,
+            category=self.category_two
+        )
+
 
     def test_search_with_query(self):
         response = self.client.get('/products/', {'q': 'Test'})
@@ -28,6 +40,19 @@ class TestProductViews(TestCase):
         response = self.client.get('/products/', {'is_best_seller': True})
         # Check returns best seller is True
         self.assertIn(self.product, response.context['products'])
+
+    def test_search_with_category(self):
+        response = self.client.get('/products/', {'category': 'Test Category'})
+        # Check returns best seller is True
+        self.assertIn(self.product, response.context['products'])
+
+    def test_sort(self):
+        response = self.client.get('/products/?sort=price&direction=asc')
+        # Check returns best seller is True
+        self.assertQuerysetEqual(response.context['products'], Product.objects.all().order_by('price'),transform=lambda x: x)
+        response_desc = self.client.get('/products/?sort=category&direction=desc')
+        # Check returns best seller is True
+        self.assertQuerysetEqual(response_desc.context['products'], Product.objects.all().order_by('-category'),transform=lambda x: x)
 
     def test_products_list_render(self):
         response = self.client.get('/products/')
