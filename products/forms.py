9d474a068623f67ae6472b35cbbefd71394d77cc
David Bowles <david.bowles84272@outlook.com>
2024-02-15 19:56:19 +0000

    Fix: indentation issue

--- products/forms.py
+++ products/forms.py
@@ -14,7 +14,7 @@ class ProductForm(forms.ModelForm):
             # list comprehension
             friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
 
-        self.fields['category'].choices = friendly_names
-        # match theme of the rest of the store
-        for field_name, field in self.fields.items():
-            field.widget.attrs['class'] = 'border-black rounded-0'
+            self.fields['category'].choices = friendly_names
+            # match theme of the rest of the store
+            for field_name, field in self.fields.items():
+                field.widget.attrs['class'] = 'border-black shadow rounded-0'
