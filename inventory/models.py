c17901e29de4df82d72b8a8ef51707129ae46aa4
David Bowles <david.bowles84272@outlook.com>
2024-02-20 10:32:24 +0000

    Added inventory app

---
+++ inventory/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+from products.models import products
+
+
+class InventoryItem(models.Model):
+    product = models.OneToOneField(Product, on_delete=models.CASCADE)
+    min_threshold = models.PositiveIntegerField(default=20)
+    min_order_quantity = models.PositiveIntegerField(default=100)
+    supplier_name = models.CharField(max_length=100, blank=True)
+    supplier_email = models.EmailField(max_length=254, blank=True)
+    last_reorder_date = models.DateField(null=True, blank=True)
+    is_expecting_delivery = models.BooleanField(default=False)
+    cost_per_product = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    total_units_sold = models.PositiveIntegerField(default=0)
+    total_lost_or_damaged = models.PositiveIntegerField(default=0)
+    total_revenue_generated = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+
+    def __str__(self):
+        return f"Inventory Item for {self.product.name}"
+
+    def is_below_threshold(self):
+        return self.product.quantity < self.threshold
